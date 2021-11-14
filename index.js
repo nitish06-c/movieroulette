0d8165c3c9655d3f891223582b342d64a36d6b07
William Linzan <williamlinzan@gmail.com>
2021-11-14 00:05:28 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-//const express = require('express');
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 
@@ -15,13 +14,10 @@ app.get('/', (req,res) => {
     res.send('Home Directory');
 });
 
-// async fetch
-
 const get_data = async url => {
     try {
       const response = await fetch(url);
       const json = await response.json();
-      //console.log(json);
       return json;
     } catch (error) {
       console.log(error);
@@ -32,34 +28,52 @@ app.get('/fight', async (req,res) => {
     const url = API_SK + API_KEY + '/fight';
     const data = await get_data(url);
     const item = data.items;
-    //console.log(item);
     const movie = item[Math.floor(Math.random() * item.length)];
     console.log(movie);
     res.send(movie);
 });
 
-app.get('/comedy', (req, res) => {
-    res.send('COMEDY');
+app.get('/comedy', async (req, res) => {
+    const url = API_SK + API_KEY + '/laugh';
+    const data = await get_data(url);
+    const item = data.items;
+    const movie = item[Math.floor(Math.random() * item.length)];
+    console.log(movie);
+    res.send(movie);
 });
 
 app.get('/drama', async (req, res) => {
     const url = API_SK + API_KEY + '/dramas';
     const data = await get_data(url);
     const item = data.items;
-    //console.log(item);
     const movie = item[Math.floor(Math.random() * item.length)];
     console.log(movie);
     res.send(movie);
 });
 
-app.get('/fantasy', (req, res) => {
-    res.send('FANTASY');
+app.get('/fantasy', async (req, res) => {
+    const url = API_SK + API_KEY + '/fantasy';
+    const data = await get_data(url);
+    const item = data.items;
+    const movie = item[Math.floor(Math.random() * item.length)];
+    console.log(movie);
+    res.send(movie);
 });
-app.get('/horror', (req, res) =>{
-    res.send('HORROR');
+app.get('/horror', async (req, res) =>{
+    const url = API_SK + API_KEY + '/spooky';
+    const data = await get_data(url);
+    const item = data.items;
+    const movie = item[Math.floor(Math.random() * item.length)];
+    console.log(movie);
+    res.send(movie);
 });
-app.get('/romance', (req, res) => {
-    res.send('ROMANCE');
+app.get('/romance', async (req, res) => {
+    const url = API_SK + API_KEY + '/courtship';
+    const data = await get_data(url);
+    const item = data.items;
+    const movie = item[Math.floor(Math.random() * item.length)];
+    console.log(movie);
+    res.send(movie);
 });
  
 app.listen(PORT, () => console.log(`Server is running on port: http:localhost:${PORT}`));
